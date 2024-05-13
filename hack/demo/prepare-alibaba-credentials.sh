ec0b2313c4d6d27b5169ec3c35a5bcf5f9110213
Henry Zeng <zeng_zh@foxmail.com>
2024-05-13 13:48:36 -0700

    fix secret generating shell
    Signed-off-by: Henry Zeng <zeng_zh@foxmail.com>

--- hack/demo/prepare-alibaba-credentials.sh
+++ hack/demo/prepare-alibaba-credentials.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "accessKeyId: ${ALICLOUD_ACCESS_KEY}\naccessKeySecret: ${ALICLOUD_SECRET_KEY}" > alibaba-credentials.conf
-kubectl create secret generic alibaba-account-creds -n crossplane-system --from-file=credentials=alibaba-credentials.conf
-rm -f alibaba-credentials.conf
+kubectl create secret generic alibaba-account-creds -n crossplane-system \
+    --from-literal=accessKeyId=${ALICLOUD_ACCESS_KEY} \
+    --from-literal=accessKeySecret=${ALICLOUD_SECRET_KEY}
